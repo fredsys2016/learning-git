55dd055314011fb1c5eadf23e1bfc8a435e4ce8d
fredsys <fredsys2016@gmail.com>
2022-03-19 12:20:07 -0400

    modified two files index, initial

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log("Hello world");
+console.log("hello dos");
 let li = 25 +25;
+let num = 100;
 console.log (li);
+console.log(num);
--- initial.js
+++ initial.js
@@ -1,2 +1,5 @@
+let num2 = 200;
 let n= 1;
+let num2 = 2;
 console.log(n+" is the world");
+console.log("hello Bolivia");
